7bde943bdd402df943259805ed41c7ce06f173f3
Bianca <biancabramati2@gmail.com>
2025-06-14 13:26:32 -0300

    fix de commit anterior

---
+++ py/token_embeddings.py
@@ -0,0 +1,14 @@
+from transformers import BertTokenizer
+
+def get_multilingual_token_embedding(token: str, tokenizer: BertTokenizer, model):
+    """
+    Devuelve el embedding (estático) para el token.
+    """
+    token_id = tokenizer.convert_tokens_to_ids(token)
+    if token_id is None or token_id == tokenizer.unk_token_id:
+        print(f"❌ El token '{token}' no pertenece al vocabulario de multilingual BERT.")
+        return None
+    embedding_vector = model.embeddings.word_embeddings.weight[token_id]
+    print(f"✅ Token: '{token}' | ID: {token_id}")
+    print(f"Embedding shape: {embedding_vector.shape}")
+    return embedding_vector
