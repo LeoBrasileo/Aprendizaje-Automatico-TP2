2da1bec16636534345d67b1cbb49812079db653e
LeoBrasileo <leobraginski@gmail.com>
2025-06-25 19:16:19 -0300

    RF por conteto de ventana

--- py/modelos/RF_Global.py
+++ py/modelos/RF_Global.py
@@ -1,9 +1,11 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 
 class RF_Global():
-    def __init__(self, n_estimators=100, random_state=98):
+    def __init__(self, n_estimators=100, random_state=98, window_size=3):
+        self.window_size = max(window_size, 1)
         self.modelo_puntuacion_inic = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
         self.modelo_puntuacion_final = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
         self.modelo_capitalizacion = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
@@ -27,10 +29,18 @@ class RF_Global():
             features = {
                 'token_id': entry['token_id'],
                 'instance_id': instance_id,
-                'is_first_token': int(instance_id != ult_instancia),
-                'previous_token_id': dataset[i-1]['token_id'] if i > 0 else 0,
-                'next_token_id': dataset[i+1]['token_id'] if i < length - 1 else 0
+                'is_first_token': int(instance_id != ult_instancia)
             }
+            for j in range(1, self.window_size + 1):
+                if i - j >= 0:
+                    features[f'previous_token_{j}'] = dataset[i - j]['token_id']
+                else:
+                    features[f'previous_token_{j}'] = 0
+
+                if i + j < length:
+                    features[f'next_token_{j}'] = dataset[i + j]['token_id']
+                else:
+                    features[f'next_token_{j}'] = 0
 
             X.append(list(features.values()))
             y_caps.append(entry['capitalizacion'])
@@ -59,3 +69,21 @@ class RF_Global():
         self.y_punt_final_pred = self.modelo_puntuacion_final.predict(X)
 
         return self.y_caps_pred, self.y_punt_inicial_pred, self.y_punt_final_pred
+    
+    def score(self, val_data):
+        X, y_caps_true, y_punt_inic_true, y_punt_fin_true = self.datasetToMatrix(val_data)
+        
+        y_caps_pred = self.modelo_capitalizacion.predict(X)
+        y_punt_inic_pred = self.modelo_puntuacion_inic.predict(X)
+        y_punt_fin_pred = self.modelo_puntuacion_final.predict(X)
+
+        # ignoramos las clases con 0s porque llevan a un accuracy engañoso.
+        mask_caps = y_caps_true != 0
+        mask_punt_inic = y_punt_inic_true != 0
+        mask_punt_fin = y_punt_fin_true != 0
+
+        score_caps = accuracy_score(y_caps_true[mask_caps], y_caps_pred[mask_caps]) if np.any(mask_caps) else None
+        score_punt_inic = accuracy_score(y_punt_inic_true[mask_punt_inic], y_punt_inic_pred[mask_punt_inic]) if np.any(mask_punt_inic) else None
+        score_punt_fin = accuracy_score(y_punt_fin_true[mask_punt_fin], y_punt_fin_pred[mask_punt_fin]) if np.any(mask_punt_fin) else None
+
+        return score_caps, score_punt_inic, score_punt_fin
